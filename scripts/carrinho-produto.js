1b90fc9a886a2b1888bec578035d6b7e95bf755a
BrenoRaeder <brenoraeder97@gmail.com>
2023-05-02 19:59:12 -0300

    estilizando e fazendo script para parte de carrinho da tela de produto

---
+++ scripts/carrinho-produto.js
@@ -0,0 +1,26 @@
+const btnMais = document.querySelector('#mais');
+const btnMenos = document.querySelector('#menos');
+const qtdTxt = document.querySelector('.quantidade p');
+const precoTxt = document.querySelector('#preco');
+
+let qtd = 1;
+let precoInicial = parseFloat(precoTxt.textContent.replace(' R$', ''));
+let precoAtual = precoInicial;
+
+btnMais.addEventListener('click', () => {
+  qtd ++;
+  precoAtual += precoInicial;
+
+  precoTxt.innerText = `${precoAtual.toFixed(2)} R$`;
+  qtdTxt.innerText = qtd;
+}) 
+
+btnMenos.addEventListener('click', () => {
+  if(qtd > 1) {
+    qtd --;
+    precoAtual -= precoInicial;
+  }
+
+  precoTxt.innerText = `${precoAtual.toFixed(2)} R$`;
+  qtdTxt.innerText = qtd;
+})

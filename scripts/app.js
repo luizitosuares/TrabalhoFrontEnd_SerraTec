e435f9f774ddf46b82ef5932fe27e570ad3718b9
BrenoRaeder <brenoraeder97@gmail.com>
2023-05-05 08:55:15 -0300

    resolvendo questões de responsividade

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 var btnSignin = document.querySelector("#signin");
 var btnSignup = document.querySelector("#signup");
+var btnLogin = document.querySelector('.login')
 
 var body = document.querySelector("body");
 
@@ -10,4 +11,10 @@ btnSignin.addEventListener("click", function () {
 
 btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
+});
+
+btnLogin.addEventListener('click', () => {
+  let listaCarrinho = []; 
+  localStorage.setItem('listaCarrinho', JSON.stringify(listaCarrinho));
+  location.href = '../home-page/home.html';
 })
